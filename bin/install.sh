3a9f45b1f917e0d407b3082196070b9a1687a580
Jorge E. Fonseca A <filipok@gmail.com>
2021-08-18 13:23:46 -0500

    Initial

---
+++ bin/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+## URL
+installer_repository="https://github.com/jefonseca/sp-installer.git"
+installer_repository_directory="sp-installer"
+
+check_result () {
+	if [[ $1 -ne 0 ]]; then
+		echo "$2"
+		exit 1
+	fi
+}
+
+## Install ansible if needed
+dpkg -s ansible >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+	apt update ; apt -y install ansible
+	check_result $? "Cannot install ansible"
+fi
+if [[ ! $(which ansible-playbook) ]]; then
+	echo "FATAL: \"ansible-playbook\" was not found in the system"
+	exit 1
+fi
+
+## Install git if needed
+dpkg -s git >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+	apt update ; apt -y install git
+	check_result $? "Cannot install git"
+fi
+if [[ ! $(which git) ]]; then
+	echo "FATAL: \"git\" was not found in the system"
+	exit 1
+fi
+
+if [ ! -d "$installer_repository_directory" ]; then
+	## clone repository
+	git clone $installer_repository $installer_repository_directory
+	check_result $? "Cannot clone repository"
+else
+	echo "Repository directory \"installer_repository_directory\" exists"
+fi
+
+## Run playbook
+ANSIBLE_LOCALHOST_WARNING=false ansible-playbook installer_repository_directory/install.yml
+check_result $? "Playbook failed"
